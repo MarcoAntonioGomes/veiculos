9f739e038c446b8cafc91dbc04c523069af2f06f
marco <marco@joinder.me>
2023-04-01 11:35:17 -0300

    Remoção do Swagger e Adição do Actuator

--- src/main/java/br/edu/infnet/veiculos/VeiculosApplication.java
+++ src/main/java/br/edu/infnet/veiculos/VeiculosApplication.java
@@ -3,6 +3,8 @@ package br.edu.infnet.veiculos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class VeiculosApplication {
 
